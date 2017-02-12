c6f1e41a0b1ace1200204ca5ae56390985b809e3
baouz06 <baouz.khaled@gmail.com>
2017-02-12 13:34:26 +0100

    question 3 et 4

--- js/pokedex.js
+++ js/pokedex.js
@@ -1,8 +1,39 @@
-var pokeApp = angular.module('pokedex', ['ngResource']);
+var pokeApp = angular.module('pokedex', [ 'ngResource' ]);
 
 // With this you can inject POKEAPI url wherever you want
 pokeApp.constant('POKEAPI', 'http://pokeapi.co');
 
-pokeApp.config(['$resourceProvider', function($resourceProvider) {
-    $resourceProvider.defaults.stripTrailingSlashes = false;
-}]);
+pokeApp.config([ '$resourceProvider', function($resourceProvider) {
+	$resourceProvider.defaults.stripTrailingSlashes = false;
+} ]);
+
+var app = angular.module('myApp', []);
+app.controller('PokeController', function($scope) {
+	console.log($scope);
+	$scope.listPoke = [ {
+		"id" : 1,
+		"name" : "Pikachu"
+
+	}, 
+	{
+		"id" : 2,
+		"name" : "Bulbizarre"
+	}, 
+	{
+		"id" : 3,
+		"name" : "Carabaffe"
+	},
+	{
+		"id" : 4,
+		"name" : "Tortank"
+	},
+	{
+		"id" : 5,
+		"name" : "Caninos"
+	}
+
+	];
+
+	
+});
+
