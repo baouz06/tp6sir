a1b244a9b802194d2f479f96f84aa7600305e49c
baouz06 <baouz.khaled@gmail.com>
2017-02-26 18:05:00 +0100

    affichage des informations retenues en utilisant factory:question 11 et 12

--- js/pokedex.js
+++ js/pokedex.js
@@ -1,22 +1,51 @@
-var pokeApp = angular.module('pokedex', [ 'ngResource' ]);
+var pokeApp = angular.module('pokedex', ['ngResource' ]);
 
-// With this you can inject POKEAPI url wherever you want
+//With this you can inject POKEAPI url wherever you want
 pokeApp.constant('POKEAPI', 'http://pokeapi.co');
 
 pokeApp.config([ '$resourceProvider', function($resourceProvider) {
 	$resourceProvider.defaults.stripTrailingSlashes = false;
 } ]);
 
-var app = angular.module('myApp', []);
+//création d'un service factory 
+pokeApp.factory('pokefactory', function($resource) {
+	return $resource('http://pokeapi.co/api/v2/pokemon/:id'); 
+});
 
-app.controller('PokeController',['$scope', '$log','$http', function($scope, $log,$http) {
+pokeApp.service('pokeService', function($resource) {
+
+	this.setId = function(idp){
+		this.id =idp
+	};
+	this.getId = function(){
+		return this.id;
+	};
+	this.setName = function(name2){
+		this.name =name2
+	};
+	this.getName = function(){
+		return this.name;
+	};
+	
+});
+// $resource
+pokeApp.controller('pokeServiceController',function($scope,pokeService,pokefactory) {
+	
+	$scope.pake = pokefactory.get({id:6});
+	console.log($scope.pake );
+});
+
+
+
+
+pokeApp.controller('PokeController',['$scope', '$log','$http', function($scope, $log,$http) {
 	//recuperations des pokemonspar le service $http
-	 $http.get("http://pokeapi.co/api/v2/pokemon/psyduck/")
+   $http.get("http://pokeapi.co/api/v2/pokemon/psyduck/")
 	  .then(function(response) {
 	      $scope.pokemons = response.data;
 	  });
 	
-	$scope.$log = $log;
+	$scope.log = $log;
 	
 	$scope.listPoke = [ {
 		"id" : 1,
